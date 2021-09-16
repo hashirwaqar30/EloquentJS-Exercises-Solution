e2c73382b8ec72beb7fb1b3af74042e7b34488c3
hashirwaqar30 <hashirwaqar30@yahoo.com>
2021-09-16 21:46:11 +0500

    Changed Folder Names

--- Eloquent JS/Chp5 Higher-order Functions/05_higher_order/code/reduce.js
+++
@@ -1,9 +0,0 @@
-const reduce = (array, combine, start) => {
-  let current = start;
-  array.forEach((element) => {
-    current = combine(current, element);
-  });
-  return current;
-};
-
-console.log(reduce([1, 2, 3, 4], (a, b) => a + b, 0));
---
+++ Eloquent-JS/Chp5-Higher-order-Functions/05_higher_order/code/filteringArrays.js
@@ -0,0 +1,13 @@
+var SCRIPTS = require("./scripts.js");
+
+const filter = (array, test) => {
+  let passed = [];
+  for (let element of array) {
+    if (test(element)) {
+      passed.push(element);
+    }
+  }
+  return passed;
+};
+
+console.log(filter(SCRIPTS, (script) => script.living));
