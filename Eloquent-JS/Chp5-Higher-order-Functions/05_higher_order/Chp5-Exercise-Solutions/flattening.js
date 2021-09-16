4888625531a18e59e5e61406b4f00aa69b36f78b
hashirwaqar30 <hashirwaqar30@yahoo.com>
2021-09-17 02:01:57 +0500

    Added Comment in Ex5.1-flattening

--- Eloquent-JS/Chp5-Higher-order-Functions/05_higher_order/Chp5-Exercise-Solutions/flattening.js
+++ Eloquent-JS/Chp5-Higher-order-Functions/05_higher_order/Chp5-Exercise-Solutions/flattening.js
@@ -1,3 +1,6 @@
+/* Use the reduce method in combination with the concat method to “flatten” an array 
+   of arrays into a single array that has all the elements of the original arrays. */
+
 function flattening(arr) {
   return arr.reduce((a, b) => {
     return a.concat(b);
